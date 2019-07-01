e55c458e0d2f16137fd1b2beadd10069f59a4ced
Rob (Personal) <robpersonal@robertoembp15tb.lan>
2019-07-01 06:57:21 -0700

    fix api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://red-sorry-32572.herokuapp.com/',
+  API_ENDPOINT: 'https://red-sorry-32572.herokuapp.com',
   API_ENDPOINT_LOCAL: 'http://localhost:8000/api',
   TOKEN_KEY: 'thingful-client-auth-token',
   API_KEY: process.env.REACT_APP_API_KEY,
