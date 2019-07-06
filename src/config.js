684a0d1156e3081637b2885039388b307800720b
Rob (Personal) <robpersonal@robertoembp15tb.lan>
2019-07-05 19:09:14 -0700

    Make form responsive

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
-  API_ENDPOINT: 'https://red-sorry-32572.herokuapp.com/api',
+  // API_ENDPOINT: 'https://red-sorry-32572.herokuapp.com/api',
   API_ENDPOINT_LOCAL: 'http://localhost:8000/api',
-  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'thingful-client-auth-token',
   API_KEY: process.env.REACT_APP_API_KEY,
 }
