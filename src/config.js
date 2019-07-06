264234c2c5a3f55fd725d21d7dbce1dc9bc31780
Rob (Personal) <robpersonal@robertoembp15tb.lan>
2019-07-06 08:56:11 -0700

    Prep for deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export default {
-  // API_ENDPOINT: 'https://red-sorry-32572.herokuapp.com/api',
-  // API_ENDPOINT_LOCAL: 'http://localhost:8000/api',
-  API_ENDPOINT: process.env.REACT_APP_NODE_ENV === 'development' 
-    ? process.env.REACT_APP_PROD_API_KEY 
-    : process.env.REACT_APP_LOCAL_API_KEY
+  API_ENDPOINT: process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_LOCAL_API_KEY 
+    : process.env.REACT_APP_PROD_API_KEY
 }
