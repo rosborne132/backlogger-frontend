51739131341b8a234ec38640a8f53b89d317897c
Rob (Personal) <robpersonal@robertoembp15tb.lan>
2019-07-06 09:03:18 -0700

    Lint and refactor tests

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 import Game from './Game'
 
 it('renders empty given no tabs', () => {
-    const component = shallow(<Game id="20394" name="test"/>)
+    const component = shallow(<Game id={20394} title="test"/>)
     expect(component).toMatchSnapshot()
 })
